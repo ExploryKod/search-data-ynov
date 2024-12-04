5c8ab9e740fe6fd03215bdb6b532cf89fd300a06
Amaury Franssen <amauryfranssen@gmail.com>
2024-12-04 01:41:10 +0100

    feat(welcome): contenu changé

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -57,7 +57,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         <main className="mt-6">
                             <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
                                 <a
-                                    href="https://laravel.com/docs"
+                                    href="https://www.algolia.com/doc/framework-integration/laravel/getting-started/introduction-to-scout-extended/"
                                     id="docs-card"
                                     className="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                                 >
@@ -112,7 +112,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 </a>
 
                                 <a
-                                    href="https://laracasts.com"
+                                    href="https://www.algolia.com/doc/framework-integration/laravel/getting-started/introduction-to-scout-extended/"
                                     className="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                                 >
                                 
